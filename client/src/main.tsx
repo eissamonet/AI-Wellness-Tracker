1051bd34cd222101a4b862ea83159335b0f85f84
Eissa Purnell <eissamonet@gmail.com>
2026-05-08 09:08:07 -0500

    commit changes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,8 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 
+document.documentElement.classList.add('dark')
+
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <App />
