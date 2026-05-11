f9ee3546a4c8da72672267c99e068dfc62c163e2
Eissa Purnell <eissamonet@gmail.com>
2026-05-11 18:19:17 -0500

    commit changes

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,10 +1,19 @@
+import { useState } from "react"
 
 
 const Login = () => {
+  const [state, setState] = useState('login')
+
   return (
-    <div>
-        Login
-    </div>
+    <>
+      <main className="login-page-container ">
+        <form className="login-form">
+          <h2 className="text-3xl font-medium text-gray-900 dark:text-white">
+            {state === 'login' ? 'Sign In ' : 'Sign Up'}
+          </h2>
+        </form>
+      </main>
+    </>
   )
 }
 
