4099a1ca312a91defdf9400fb8474b8450636e41
Eissa Purnell <eissamonet@gmail.com>
2026-05-11 18:30:46 -0500

    commit changes

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,20 +1,37 @@
-import { useState } from "react"
-
+import { AtSignIcon } from "lucide-react";
+import { useState } from "react";
 
 const Login = () => {
-  const [state, setState] = useState('login')
+  const [state, setState] = useState("login");
 
   return (
     <>
       <main className="login-page-container ">
         <form className="login-form">
           <h2 className="text-3xl font-medium text-gray-900 dark:text-white">
-            {state === 'login' ? 'Sign In ' : 'Sign Up'}
+            {state === "login" ? "Sign In " : "Sign Up"}
           </h2>
+          <p className="mt-2 text-sm text-gray-500/90 dark:text-gray-400">
+            {state === "login"
+              ? "Please enter your email and password to sign in."
+              : "Please fill in the form to create an account."}
+          </p>
+
+          {state !== "login" && (
+            <div>
+              <label className="font-medium text-sm text-gray-700 dark:text-grat-300">Username</label>
+              <div>
+                <AtSignIcon className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 size-4.5"/>
+                <input
+                type="text"
+                placeholder="Name" />
+              </div>
+            </div>
+          )}
         </form>
       </main>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
