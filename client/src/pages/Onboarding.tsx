4740f449602623c388d36c2618e9fb51f40eacbf
Eissa Purnell <eissamonet@gmail.com>
2026-05-18 08:23:26 -0500

    commit changes

--- client/src/pages/Onboarding.tsx
+++ client/src/pages/Onboarding.tsx
@@ -168,7 +168,7 @@ const Onboarding = () => {
         <div>
           <div>
             {step > 1 && (
-              <Button variant="secondary" onClick={()=>} className="max-lg:flex-1 lg:px-10">
+              <Button variant="secondary" onClick={()=> setStep(step > 1 ? step - 1 : 1)} className="max-lg:flex-1 lg:px-10">
                   <span className="flex items-center justify-center gap-2">
                     <ArrowLeft className="w-5 h-5" />
                   </span>
