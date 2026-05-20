a0bb3871d758b61b1c111230005b1bb98cc4a978
Eissa Purnell <eissamonet@gmail.com>
2026-05-20 09:17:28 -0500

    sidebar component added

--- client/src/pages/Layout.tsx
+++ client/src/pages/Layout.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom"
+import Sidebar from "../components/Sidebar"
 
 
 const Layout = () => {
   return (
     <div className="layout-container">
           Layout
-        <Outlet />
+        <Sidebar />
     </div>
   )
 }
