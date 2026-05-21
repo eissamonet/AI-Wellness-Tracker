d0e3f6b9b31446b0fdc1dd9d320133f5fd8516af
Eissa Purnell <eissamonet@gmail.com>
2026-05-21 08:25:08 -0500

    commit changes

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,9 +1,12 @@
-import { HomeIcon } from "lucide-react"
+import { ActivityIcon, HomeIcon, UserIcon, UtensilsIcon } from "lucide-react"
 
 const Sidebar = () => {
 
     const navItems = [
-        {path: '/', label: 'Home', icon: HomeIcon}
+        {path: '/', label: 'Home', icon: HomeIcon},
+        {path: '/food', label: 'Food', icon: UtensilsIcon},
+        {path: '/activity', label: 'Activity', icon: ActivityIcon},
+        {path: '/profile', label: 'Profile', icon: UserIcon},
     ]
 
   return (
