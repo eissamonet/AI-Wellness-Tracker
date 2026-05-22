bee12c3e5ca6fa753ca3a49c8d26188ba8f0d9be
Eissa Purnell <eissamonet@gmail.com>
2026-05-22 15:50:02 -0500

    commit changes

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -11,6 +11,16 @@ const Dashboard = () => {
        <div className="dashboard-header">
           <p className="text-emerald-100 text-sm font-medium">Welcome Back!</p>
           <h1 className="text-2xl font-bold mt-1">{`Hey there! ${user?.username}`}</h1>
+
+          {/* motivation card */}
+          <div className="mt-6 bg-white/20 backdrop-blur-sm rounded-2xl p-4">
+            <div>
+              <span></span>
+              <p></p>
+            </div>
+          </div>
+
+
        </div>
     </div>
   )
