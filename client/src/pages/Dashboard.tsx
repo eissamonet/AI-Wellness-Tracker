f4ab9c7ca3bfd30a27fed94b90f2ccaf99059b52
Eissa Purnell <eissamonet@gmail.com>
2026-05-22 15:31:29 -0500

    commit changes

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,11 +1,16 @@
+import { useAppContext } from "../context/AppContext"
 
 
 const Dashboard = () => {
+
+  const {user, allActivityLogs, allFoodLogs} = useAppContext()
+
   return (
     <div className="page-containter">
        {/* header */}
        <div className="dashboard-header">
           <p className="text-emerald-100 text-sm font-medium">Welcome Back!</p>
+          <h1 className="text-2xl font-bold mt-1">{`Hey there, ${user?.username}`}</h1>
        </div>
     </div>
   )
