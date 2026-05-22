91abc19290a2bb2acf0ef3747a4a00cc8f917061
Eissa Purnell <eissamonet@gmail.com>
2026-05-22 13:58:20 -0500

    bottom nav comp created

--- client/src/components/BottomNav.tsx
+++ client/src/components/BottomNav.tsx
@@ -1,9 +1,11 @@
+import { Activity, Home, User, Utensils } from "lucide-react";
+
 const BottomNav = () => {
   const navItems = [
-    { path: "/", label: "Home", icon: HomeIcon },
-    { path: "/food", label: "Food", icon: UtensilsIcon },
-    { path: "/activity", label: "Activity", icon: ActivityIcon },
-    { path: "/profile", label: "Profile", icon: UserIcon },
+    { path: "/", label: "Home", icon: Home },
+    { path: "/food", label: "Food", icon: Utensils },
+    { path: "/activity", label: "Activity", icon: Activity },
+    { path: "/profile", label: "Profile", icon: User },
   ];
 
   return <div></div>;
