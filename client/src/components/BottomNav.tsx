febe6c66efbd62ee2c4f9c5be0df474eb093e458
Eissa Purnell <eissamonet@gmail.com>
2026-05-22 14:18:12 -0500

    commit changes

--- client/src/components/BottomNav.tsx
+++ client/src/components/BottomNav.tsx
@@ -1,4 +1,5 @@
 import { Activity, Home, User, Utensils } from "lucide-react";
+import { NavLink } from "react-router-dom";
 
 const BottomNav = () => {
   const navItems = [
@@ -11,6 +12,14 @@ const BottomNav = () => {
   return (
   <nav className="fixed bottom-0 left-0 right-0 bg-white dark:bg-slate-900 border-t border-slate-100 dark:border-slate-800
   px-4 pb-safe lg:hidden transition-colors duration-200">
+    <div className="max-w-lg mx-auto flex justify-around items center h-16">
+        {navItems.map((item) => (
+           <NavLink>
+             <item.icon className="size-5.5" />
+           </NavLink>
+        ))}
+
+    </div>
 
   </nav>
   );
