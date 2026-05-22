f6db29b125454d2ade2903312e72f3ec935282f7
Eissa Purnell <eissamonet@gmail.com>
2026-05-22 15:28:19 -0500

    commit changes

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -5,7 +5,7 @@ const Dashboard = () => {
     <div className="page-containter">
        {/* header */}
        <div className="dashboard-header">
-
+          <p className="text-emerald-100 text-sm font-medium">Welcome Back!</p>
        </div>
     </div>
   )
