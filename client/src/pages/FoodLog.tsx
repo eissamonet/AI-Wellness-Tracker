ba8515d0ff6e869498e422469c760cee3ff86d8b
Eissa Purnell <eissamonet@gmail.com>
2026-05-28 18:27:47 -0500

    commit changes

--- client/src/pages/FoodLog.tsx
+++ client/src/pages/FoodLog.tsx
@@ -1,5 +1,10 @@
+import { useState } from "react";
+import { useAppContext } from "../context/AppContext"
 
 const FoodLog = () => {
+  const {allFoodLogs, setAllFoodLogs} = useAppContext();
+
+  const [entries, setEntries] = useState(allFoodLogs);
   return (
     <div>
        FoodLog
