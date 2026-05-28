4fdff7ce7e9ada31f3cf206762e4eed868aa370f
Eissa Purnell <eissamonet@gmail.com>
2026-05-28 18:36:54 -0500

    foodlog updated

--- client/src/pages/FoodLog.tsx
+++ client/src/pages/FoodLog.tsx
@@ -7,7 +7,7 @@ const FoodLog = () => {
 
   const [entries, setEntries] = useState<FoodEntry[]>([])
   const [showForm, setShowForm] = useState(false);
-  const [formData, setFormData] = useState({
+  const [formData, setFormData] = useState<FormData>({
     name: '',
     calories: 0,
     mealType: '',
