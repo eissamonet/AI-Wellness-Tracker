8e5f57adb7841d0965bf02fc98c426703d72bf93
Eissa Purnell <eissamonet@gmail.com>
2026-05-28 08:24:46 -0500

    styling updated

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -233,6 +233,8 @@ const Dashboard = () => {
                 <div className="h-2 w-full bg-slate-100 dark:bg-slate-800 rounded-full overflow-hidden flex">
                   <div className="flex-1 bg-blue-400 opacity-30"></div>
                   <div className="flex-1 bg-emerald-400 opacity-30"></div>
+                  <div className="flex-1 bg-oranger-400 opacity-30"></div>
+                  <div className="flex-1 bg-red-400 opacity-30"></div>
                 </div>
               </div>
             )}
