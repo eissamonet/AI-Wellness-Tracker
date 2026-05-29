92cea0ac29c26a1ee9819e24fcf14e08c9aa539d
Eissa Purnell <eissamonet@gmail.com>
2026-05-29 09:26:11 -0500

    added btn function for quick add

--- client/src/pages/FoodLog.tsx
+++ client/src/pages/FoodLog.tsx
@@ -25,6 +25,11 @@ const FoodLog = () => {
 
   const totalCalories = entries.reduce((total, entry) => total + entry.calories, 0);
 
+  const handleQuickAdd = (activityName: string) => {
+    setFormData({...formData, mealType: activityName})
+    setShowForm(true);
+  }
+
   useEffect(() => {
     (() => {
       loadEntries();
@@ -57,7 +62,7 @@ const FoodLog = () => {
               <h3 className="font-semibold text-slate-700 dark:text-slate-200 mb-3">Quick Add</h3>
               <div className=" flex flex-wrap gap-2">
                 {quickActivitiesFoodLog.map((activity)=> (
-                  <button
+                  <button onClick={}
                   className="px-4 py-2 bg-slate-100 dark:bg-slate-800 hover:bg-slate-200 dark:hover:bg-slate-700 rounded-xl
                   text-sm font-medium text-slate-700 dark:text-slate-200 transition-colors" key={activity.name}>
                     {activity.emoji} {activity.name}
