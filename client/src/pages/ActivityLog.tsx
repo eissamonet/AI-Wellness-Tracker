d11661a967783d3a02f8192b5119e43c13568fae
Eissa Purnell <eissamonet@gmail.com>
2026-06-05 08:30:17 -0500

    activity log updated

--- client/src/pages/ActivityLog.tsx
+++ client/src/pages/ActivityLog.tsx
@@ -1,8 +1,14 @@
+import { useState } from "react";
 import { useAppContext } from "../context/AppContext"
+import { ActivityEntry } from "../types";
 
 const ActivityLog = () => {
 
   const {allActivityLogs, setAllActivityLogs} = useAppContext();
+
+  const [activity, setActivity] = useState<ActivityEntry[]>([])
+  const [showForm, setShowForm] = useState(false)
+  const [formData, setFormData] = useState({name: '', duration: 0, caloriesBurned: 0})
 
   return (
     <div>
