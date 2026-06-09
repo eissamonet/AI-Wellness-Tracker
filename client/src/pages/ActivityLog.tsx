15b8902ab9a25a81d4a83b0b629e4c5f2740c9ac
Eissa Purnell <eissamonet@gmail.com>
2026-06-09 09:24:29 -0500

    styling updated

--- client/src/pages/ActivityLog.tsx
+++ client/src/pages/ActivityLog.tsx
@@ -179,7 +179,12 @@ const ActivityLog = () => {
                    </div>
                   </div>
                   <div className="flex items-center gap-3">
+                    <div>
 
+                    </div>
+                    <button className="p-2 text-red-400 hover:text-red-600 hover:bg-red-50 dark:hover:bg-red-900/20 rounded-lg transition-colors">
+                      <Trash2Icon className="w-4 h-4" />
+                    </button>
                   </div>
                 </div>
               ))}
