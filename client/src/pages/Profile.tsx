06e9c43111bc164451047b59d7b69e8eee059971
Eissa Purnell <eissamonet@gmail.com>
2026-06-10 08:50:27 -0500

    profile pg setup

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,5 +1,9 @@
+import { useAppContext } from "../context/AppContext"
+import { useTheme } from "../context/ThemeContext";
 
 const Profile = () => {
+  const {user, logout, fetchUser, allFoodLogs, allActivityLogs} = useAppContext();
+  const {theme, toggleTheme} = useTheme();
   return (
     <div>
         Profile
