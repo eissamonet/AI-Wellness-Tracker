cf6bc80f29f75720658fcb2bba46c7190ba2815a
Eissa Purnell <eissamonet@gmail.com>
2026-06-10 09:04:41 -0500

    commit changes

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import { useAppContext } from "../context/AppContext"
 import { useTheme } from "../context/ThemeContext";
 
 const Profile = () => {
   const {user, logout, fetchUser, allFoodLogs, allActivityLogs} = useAppContext();
   const {theme, toggleTheme} = useTheme();
+
+  const [isEditing, setIsEditing] = useState(false);
 
   return (
     <div>
