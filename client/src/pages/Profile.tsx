9e0fea7741e6f039c19ddeaaad85b21edf2045a1
Eissa Purnell <eissamonet@gmail.com>
2026-06-10 09:09:20 -0500

    commit changes

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -4,11 +4,11 @@ import { useTheme } from "../context/ThemeContext";
 import type { ProfileFormData } from "../types";
 
 const Profile = () => {
-  const {user, logout, fetchUser, allFoodLogs, allActivityLogs} = useAppContext();
-  const {theme, toggleTheme} = useTheme();
+  const {user, logout, fetchUser, allFoodLogs, allActivityLogs} = useAppContext()
+  const {theme, toggleTheme} = useTheme()
 
-  const [isEditing, setIsEditing] = useState(false);
-  const [formData, setFormData] = useState<ProfileFormData>({age: 0, weight: 0, height: 0, goal: 'maintain', dailyCalorieIntake: 2000, dailyCalorieBurn: 400});
+  const [isEditing, setIsEditing] = useState(false)
+  const [formData, setFormData] = useState<ProfileFormData>({age: 0, weight: 0, height: 0, goal: 'maintain', dailyCalorieIntake: 2000, dailyCalorieBurn: 400})
 
   return (
     <div>
