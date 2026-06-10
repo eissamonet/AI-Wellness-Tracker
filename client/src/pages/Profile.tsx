22a96a603f0f783bfe4da9e7c9981249b8430b8c
Eissa Purnell <eissamonet@gmail.com>
2026-06-10 09:11:01 -0500

    commit changes

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -10,6 +10,20 @@ const Profile = () => {
   const [isEditing, setIsEditing] = useState(false)
   const [formData, setFormData] = useState<ProfileFormData>({age: 0, weight: 0, height: 0, goal: 'maintain', dailyCalorieIntake: 2000, dailyCalorieBurn: 400})
 
+  const fetchUserData = () => {
+    if(user) {
+      setFormData({
+        age: user?.age || 0,
+        weight: user?.weight || 0,
+        height: user?.height || 0,
+        goal: user?.goal || 'maintain',
+        dailyCalorieIntake: user?.dailyCalorieIntake || 2000,
+        dailyCalorieBurn: user?.dailyCalorieBurn || 400
+       })
+     }
+  }
+
+
   return (
     <div>
         Profile
