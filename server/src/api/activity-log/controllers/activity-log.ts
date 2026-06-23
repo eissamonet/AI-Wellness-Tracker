0116b23522898edd148d5553a3ea370c2facbbae
Eissa Purnell <eissamonet@gmail.com>
2026-06-23 09:22:27 -0500

    edit strapi activity log controller

--- server/src/api/activity-log/controllers/activity-log.ts
+++ server/src/api/activity-log/controllers/activity-log.ts
@@ -2,20 +2,29 @@
  * activity-log controller
  */
 
-import { factories } from '@strapi/strapi';
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::activity-log.activity-log', ({ strapi }) => ({
+export default factories.createCoreController(
+  "api::activity-log.activity-log",
+  ({ strapi }) => ({
     async create(ctx) {
-       const user = ctx.state.user
+      const user = ctx.state.user;
 
-       if (!user) return ctx.unauthorized('You must be logged in to create an activity log.')
-        const body = ctx.request.body;
-    body.users_permissions_user = user.id; // Assign the logged-in user's ID to the users_permissions_user field
+      if (!user)
+        return ctx.unauthorized(
+          "You must be logged in to create an activity log.",
+        );
+      const body = ctx.request.body;
+      body.users_permissions_user = user.id; // Assign the logged-in user's ID to the users_permissions_user field
 
-    const entry = await strapi.entityService.create('api::activity-log.activity-log', {
-        data: body,
-        populate: ['users_permissions_user'], // Populate the users_permissions_user relation
-    });
-
-    }
-}));
+      const entry = await strapi.entityService.create(
+        "api::activity-log.activity-log",
+        {
+          data: body,
+          populate: ["users_permissions_user"], // Populate the users_permissions_user relation
+        },
+      );
+      return entry;
+    },
+  }),
+);
