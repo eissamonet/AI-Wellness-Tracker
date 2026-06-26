6dc529c4164a73688604e8b7a52e71931e82036f
Eissa Purnell <eissamonet@gmail.com>
2026-06-26 09:29:05 -0500

    updated food-log function

--- server/src/api/food-log/controllers/food-log.ts
+++ server/src/api/food-log/controllers/food-log.ts
@@ -13,18 +13,20 @@ export default factories.createCoreController('api::food-log.food-log',
         return ctx.unauthorized(
           "You must be logged in to create an activity log.",
         );
-      const body = ctx.request.body;
-      body.users_permissions_user = user.id; // Assign the logged-in user's ID to the users_permissions_user field
+        const { name, mealType, calories } = ctx.request.body.data;
 
-      const entry = await strapi.entityService.create(
-        "api::food-log.food-log",
-        {
-          data: body,
-          populate: ["users_permissions_user"], // Populate the users_permissions_user relation
+    const entry = await strapi.entityService.create("api::food-log.food-log", {
+        data: {
+            name,
+            mealType,
+            calories,
+            users_permissions_user: user.id, // ✅ attach user correctly
         },
-      );
-      return entry;
-    },
+        populate: ["users_permissions_user"],
+    });
+
+    return entry;
+},
     async find(ctx) {
       const user = ctx.state.user;
 
