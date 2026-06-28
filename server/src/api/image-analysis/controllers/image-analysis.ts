67583d4c8999631b019b4250d60d423b2dc55a8a
Eissa Purnell <eissamonet@gmail.com>
2026-06-28 15:04:55 -0500

    commit changes

--- server/src/api/image-analysis/controllers/image-analysis.ts
+++ server/src/api/image-analysis/controllers/image-analysis.ts
@@ -4,5 +4,14 @@ import { Context } from 'koa';
 export default {
     async analyze(ctx: Context){
       const file = ctx.request.files?.image as any;
+      if(!file) return ctx.badRequest('No image uploaded');
+
+      const filePath = file.filepath;
+
+      try {
+
+      } catch (error) {
+
+      }
     }
 }
