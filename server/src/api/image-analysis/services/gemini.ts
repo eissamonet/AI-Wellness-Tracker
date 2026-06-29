8d1f23d2fce38170ab3185d4454e1efa7580d7b8
Eissa Purnell <eissamonet@gmail.com>
2026-06-29 10:01:48 -0500

    gemini added

--- server/src/api/image-analysis/services/gemini.ts
+++ server/src/api/image-analysis/services/gemini.ts
@@ -1,5 +1,10 @@
 import { GoogleGenAI } from "@google/genai";
+import fs from "fs";
+
+const client = new GoogleGenAI({});
 
 export const analyzeImage = async (filePath: string) => {
-
-}
+  const base64ImageFile = fs.readFileSync(filePath, {
+    encoding: "base64",
+  });
+};
